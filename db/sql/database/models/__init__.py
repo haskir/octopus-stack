042a4714865c59353c62423f809486f3d7d74543
haskir <haskird2@gmail.com>
2025-04-25 12:59:45 +0300

    clearing

--- db/sql/database/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .model_examples import User
-from .token import Token
-from .challenge import Challenge
-from .account import Account, UserPreferences
-from .transaction import *
--- db/sql/database/repository/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user_repo import UserRepo
-from .challenge_repo import ChallengeRepo
-from .token_repo import TokenRepo
-from .transaction_repo import TransactionRepo
-from .preferences_repo import PreferencesRepo
-from .report_repo import ReportRepo
-from .account_repo import AccountRepo
